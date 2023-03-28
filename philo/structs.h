6f55fdc2515b215a7e6f7c700743004e8dbdd060
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-03-28 10:27:25 +0200

    Feat: Add structs for the table and for each philosopher

--- philo/structs.h
+++ philo/structs.h
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 18:49:04 by victofer          #+#    #+#             */
-/*   Updated: 2023/03/22 11:41:35 by victofer         ###   ########.fr       */
+/*   Updated: 2023/03/28 10:26:04 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,40 @@
 
 //   S T R U C T U R E S
 
-typedef struct s_vars
+typedef struct s_philo
 {
+	pthread_t		thread;
+	int				id;
+	int				times_ate;
+	int				*forks;
+	pthread_mutex_t	meal_lock;
+	time_t			last_meal;
+	t_table			table;
+}	t_philo;
+
+typedef struct s_table
+{
+	int				time_start;
 	int				nb_philo;
-	int				nb_forks;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
+	time_t			time_to_die;
+	time_t			time_to_eat;
+	time_t			time_to_sleep;
 	int				times_x_eat;
-	pthread_mutex_t	mutex;
-}					t_vars;
+	int				simulation_stop;
+	pthread_mutex_t	sim_stop_lock;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	**fork_locks;
+	t_philo			**philos;
+}	t_table;
+
+typedef enum e_status
+{
+	DEAD = 0,
+	EATING = 0,
+	SLEEPING = 0,
+	THINKING = 0,
+	FORK_1 = 0,
+	FORK_2 = 0
+}	t_status;
 
 #endif
