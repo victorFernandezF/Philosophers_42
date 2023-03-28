776df79d96edeafbde658fc8e996aead2eccd341
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-03-28 12:46:18 +0200

    Feat: Add function to asign forks to philos

--- philo/init_structs.c
+++ philo/init_structs.c
@@ -6,11 +6,32 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:39:40 by victofer          #+#    #+#             */
-/*   Updated: 2023/03/28 12:14:20 by victofer         ###   ########.fr       */
+/*   Updated: 2023/03/28 12:43:48 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+/* 
+ * asign_forks
+ * ----------------------------
+ *	Asign two forks to each philosopher.
+ *	The objective of the function is to avoid deathlocks
+ *	(situations in which every philosopher take a fork but any of them
+ *	is able to eat so everyone die like stupids.)
+ * 
+ *	philos: struct with each philosopher datas.
+ */
+void	asign_forks(t_philo *philos)
+{
+	philos->forks[0] = philos->id;
+	philos->forks[1] = (philos->id + 1) % (philos->table->nb_philo);
+	if (philos->id % 2)
+	{
+		philos->forks[0] = (philos->id + 1) % (philos->table->nb_philo);
+		philos->forks[1] = philos->id;
+	}
+}
 
 /* 
  * init_philos
@@ -39,7 +60,7 @@ t_philo	**init_philos(t_table *table)
 		philos[i]->table = table;
 		philos[i]->times_ate = 0;
 		philos[i]->forks = malloc(sizeof(int) * 2);
-		//asign_forks(philos[i]);
+		asign_forks(philos[i]);
 		i++;
 	}
 	return (philos);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 18:48:21 by victofer          #+#    #+#             */
-/*   Updated: 2023/03/28 11:38:57 by victofer         ###   ########.fr       */
+/*   Updated: 2023/03/28 12:44:48 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@
 long long	ft_atoi(const char *str);
 t_table		*init_table(t_table *table, int argc, char **argv);
 t_philo		**init_philos(t_table *table);
+void		asign_forks(t_philo *philos);
 long int	get_timestamp_ms(void);
 //int			create_philo(t_vars *vars);
 void		*routine(void *args);
