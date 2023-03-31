80e12acfcd6659f691dcc0080354f4537a765905
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-03-31 10:42:39 +0200

    Feat: Create a new routine for the dead-checker thread.

--- philo/threads_mutex/thread.c
+++ philo/threads_mutex/thread.c
@@ -6,17 +6,28 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:43:43 by victofer          #+#    #+#             */
-/*   Updated: 2023/03/31 10:37:06 by victofer         ###   ########.fr       */
+/*   Updated: 2023/03/31 10:41:00 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
 
 static void	*sim(void	*arg)
 {
 	pthread_t	id;
 
 	id = (pthread_t)arg;
+	printf("Hello i'm a thread\n");
+	return (NULL);
+}
+
+static void	*dead(void	*arg)
+{
+	pthread_t	id;
+
+	id = (pthread_t)arg;
+	printf("I'M DEAD CHECKER\n");
 	return (NULL);
 }
 
@@ -44,7 +55,7 @@ int	start_philosophers(t_table	*table)
 	}
 	if (table->nb_philo > 1)
 	{
-		if (pthread_create(&table->dead_checker, NULL, &sim, table) != 0)
+		if (pthread_create(&table->dead_checker, NULL, &dead, table) != 0)
 		{
 			print_error_msg(THREAD_ERROR, "fail creating dead_checker thread");
 			return (0);
