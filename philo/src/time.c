ad8f690f4a54d6ae2c057d1569c86313de19ff45
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-04-05 12:35:25 +0200

    Feat: add delay function

--- philo/src/time.c
+++ philo/src/time.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 18:32:34 by victofer          #+#    #+#             */
-/*   Updated: 2023/04/03 10:10:01 by victofer         ###   ########.fr       */
+/*   Updated: 2023/04/05 11:02:19 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,10 @@ time_t	get_timestamp_ms(void)
 
 	gettimeofday(&cur, NULL);
 	return ((cur.tv_sec * 1000) + (cur.tv_usec / 1000));
+}
+
+void	delay(t_table *table)
+{
+	while (get_timestamp_ms() < table->time_start)
+		continue ;
 }
